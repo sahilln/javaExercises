1f970997a218d2c889342f2cf0bdfc43faf45679
Sahill Naik <snaik1389@gmail.com>
2017-05-23 13:52:24 -0400

    Final W2D5 Submission

--- m1-w2d5-encapsulation/src/main/java/com/techelevator/FruitTree.java
+++ m1-w2d5-encapsulation/src/main/java/com/techelevator/FruitTree.java
@@ -1,29 +1,30 @@
-package com.techelevator;
+package com.techelevator; 
 
-public class FruitTree {
-	private String typeOfFruit;
-	private int piecesOfFruitLeft;
-	private int startingPiecesOfFruit;
+	public class FruitTree { 
+		private String typeOfFruit; 
+		private int piecesOfFruitLeft; 
+
+		public String getTypeOfFruit(){ 
+			return typeOfFruit; 
+		} 
 	
-	public FruitTree(String typeOfFruit, int startingPiecesOfFruit){
-		this.typeOfFruit = typeOfFruit;
-		this.startingPiecesOfFruit = startingPiecesOfFruit;
+		public int getPiecesOfFruitLeft(){ 
+			return piecesOfFruitLeft; 
+		} 
+
+		public boolean pickFruit(int numberOfPiecesToRemove){ 
+			
+			if(numberOfPiecesToRemove > 0 && this.piecesOfFruitLeft > 0){ 
+				this.piecesOfFruitLeft -=numberOfPiecesToRemove; 
+				return true; 
+			} 
+			
+			return false; 
+		} 
+		
+		public FruitTree(String typeOfFruit, int startingPiecesOfFruit){ 
+			this.typeOfFruit = typeOfFruit; this.piecesOfFruitLeft = startingPiecesOfFruit; 
+		} 
 	}
-	
-	public String getTypeOfFruit(){
-		return typeOfFruit;
-	}
-	
-	public int getPiecesOfFruitLeft(){
-		return piecesOfFruitLeft;
-	}
-	
-	public boolean pickFruit(int numberOfPiecesToRemove){
-		if(piecesOfFruitLeft < 1){
-			return false;
-		}
-		else{
-			return true;
-		}
-	}
-}
+
+
