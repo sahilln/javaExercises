8dbf6a4413b7f1b1fbc0678488f114007a8930e2
Sahill Naik <snaik1389@gmail.com>
2017-06-08 08:54:36 -0400

    m2w5d2 final

--- m2-w5d3-joins/joins-exercises.sql
+++ m2-w5d3-joins/joins-exercises.sql
@@ -3,32 +3,93 @@
 -- 1. All of the films that Nick Stallone has appeared in
 -- (30 rows)
 
+SELECT film.title 
+FROM actor
+JOIN film_actor ON actor.actor_id = film_actor.actor_id
+JOIN film ON film_actor.film_id = film.film_id
+WHERE first_name = 'NICK' AND last_name = 'STALLONE';
+
 -- 2. All of the films that Rita Reynolds has appeared in
 -- (20 rows)
+
+SELECT film.title
+FROM actor
+JOIN film_actor ON actor.actor_id = film_actor.actor_id
+JOIN film ON film_actor.film_id = film.film_id
+WHERE first_name = 'RITA' AND last_name = 'REYNOLDS';
 
 -- 3. All of the films that Judy Dean or River Dean have appeared in
 -- (46 rows)
 
+SELECT film.title
+FROM actor
+JOIN film_actor ON actor.actor_id = film_actor.actor_id
+JOIN film ON film_actor.film_id = film.film_id
+WHERE (first_name = 'JUDY' AND last_name = 'DEAN') OR (first_name = 'RIVER' AND last_name = 'DEAN');
+
+
 -- 4. All of the the ‘Documentary’ films
 -- (68 rows)
+
+SELECT film.title, category.name
+FROM category
+JOIN film_category ON category.category_id = film_category.category_id
+JOIN film ON film_category.film_id = film.film_id
+WHERE name = 'Documentary';
 
 -- 5. All of the ‘Comedy’ films
 -- (58 rows)
 
+SELECT film.title, category.name
+FROM category
+JOIN film_category ON category.category_id = film_category.category_id
+JOIN film ON film_category.film_id = film.film_id
+WHERE name = 'Comedy';
+
 -- 6. All of the ‘Children’ films that are rated ‘G’
 -- (10 rows)
+
+SELECT film.title
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE film.rating = 'G' AND category.name = 'Children';
 
 -- 7. All of the ‘Family’ films that are rated ‘G’ and are less than 2 hours in length
 -- (3 rows)
 
+SELECT film.title, film.length, film.rating
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE film.rating = 'G' AND category.name = 'Family' AND length < 120;
+
 -- 8. All of the films featuring actor Matthew Leigh that are rated ‘G’
 -- (9 rows)
+
+SELECT film.title, film.rating
+FROM actor
+JOIN film_actor ON actor.actor_id = film_actor.actor_id
+JOIN film ON film_actor.film_id = film.film_id
+WHERE first_name = 'MATTHEW' AND last_name = 'LEIGH' AND film.rating = 'G';
 
 -- 9. All of the ‘Sci-Fi’ films released in 2006
 -- (61 rows)
 
+SELECT film.title
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE film.release_year = '2006' AND category.name = 'Sci-Fi';
+
 -- 10. All of the ‘Action’ films starring Nick Stallone
 -- (2 rows)
+
+SELECT film.title
+FROM film
+
+
+WHERE actor.first_name = 'NICK' AND actor.last_name = 'Stallone' AND category.name = 'Action';
 
 -- 11. The address of all stores, including street address, city, district, and country
 -- (2 rows)
@@ -47,6 +108,14 @@
 
 -- 16. The top ten film titles by number of rentals
 -- (#1 should be “BUCKET BROTHERHOOD” with 34 rentals and #10 should have 31 rentals)
+
+SELECT film.title, count(*) as count
+FROM rental
+JOIN inventory ON rental.inventory_id = inventory.inventory_id
+JOIN film ON inventory.film_id = film.film_id
+GROUP BY film.film_id
+ORDER BY count DESC
+LIMIT 10;
 
 -- 17. The top five film categories by number of rentals 
 -- (#1 should be “Sports” with 1179 rentals and #5 should be “Family” with 1096 rentals)
