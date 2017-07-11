b39dbd3dfa10663b6f918f7369ad09fb32897b61
Sahill Naik <sahillnaik@Sahills-MacBook-Pro.local>
2017-07-11 08:04:44 -0400

    test

--- m4-w10d1-intro-to-js/challenge-exercises.js
+++ m4-w10d1-intro-to-js/challenge-exercises.js
@@ -1,1 +1,38 @@
-﻿
+﻿function iqTest(str){
+	var numArray = str.split(" ");
+	var result;
+	var countEven = 0;
+	var countOdd = 0;
+	if(str.length == 0){
+		return 0;
+	}
+	else{
+		for(var i = 0; i < numArray.length; i++){
+			if(numArray[i] % 2 == 0){
+				countEven++;
+			}
+			if(numArray[i] % 2 != 0){
+				countOdd++;
+			}
+		}
+		if(countOdd == 0 || countEven == 0){
+			return 0;
+		}
+		if(countOdd > 1){
+			for(var i = 0; i < numArray.length; i++){
+				if(numArray[i] % 2 == 0){
+					result = numArray[i];
+				}
+			}
+		}
+		else{
+			for(var i = 0; i <numArray.length; i++){
+				if(numArray[i] % 2 != 0){
+					result = i + 1;
+				}
+			}
+		}
+	}
+	return result;
+}
+
