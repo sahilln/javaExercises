31b09bd5b572ab9dafa17d1c2519de1167bf6097
Sahill Naik <snaik1389@gmail.com>
2017-07-12 19:55:38 -0400

    test

--- m4-w10-history-geek/src/main/webapp/js/exercises/event-exerises.js
+++ m4-w10-history-geek/src/main/webapp/js/exercises/event-exerises.js
@@ -2,4 +2,34 @@
 
 $(document).ready(function () {
 
+	var sameShipping = $("#SameShipping");
+	sameShipping.on("change", function(event){
+		if(sameShipping.is(':checked')){
+			$("#ShippingAddress1").val($("#BillingAddress1").val());
+			$("#ShippingAddress2").val($("#BillingAddress2").val());
+			$("#ShippingCity").val($("#BillingCity").val());
+			$("#ShippingState").val($("#BillingState").val());
+			$("#ShippingPostalCode").val($("#BillingPostalCode").val());
+		}
+	});
+	
+	var shippingMethod = $('input[name="ShippingType');
+	shippingMethod.on("change", function (event) {
+		var shippingCost = parseFloat($(this).attr('data-cost'));
+		$('#shipping .price').html('$' + shippingCost);
+		
+		
+		var subtotal = parseFloat($('#subtotal .price').html().substring(1));
+	
+		$('#grandtotal .price').html('$' + (subtotal + shippingCost));
+	});
+	
+	var gameCell = $("#gameCell");
+	gameCell.on("keydown", function (event){
+		if($(this).val() == )
+		$('#gameboard').children("#gamecell").next().addClass("ship");
+	});
+	
+	
+
 });
