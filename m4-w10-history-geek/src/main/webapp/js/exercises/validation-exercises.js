4884a6f9a47336174e8d6ec327e329ca5dfb0420
Sahill Naik <snaik1389@gmail.com>
2017-07-13 15:45:53 -0400

    test

--- m4-w10-history-geek/src/main/webapp/js/exercises/validation-exercises.js
+++ m4-w10-history-geek/src/main/webapp/js/exercises/validation-exercises.js
@@ -2,6 +2,40 @@
 /// <reference path="../jquery.validate.js" />
 
 $(document).ready(function () {
+	
+	$("#form0").validate({
+
+        debug: false,
+        rules: {
+        	email: {
+        		required: true,
+        	},
+			password: {
+				required: true,
+			},
+			confirmPassword: {
+				required: true,
+			}
+        },
+        
+		messages: {
+			email: {
+                required: "You must provide a valid email."
+            },
+        	password: {
+        		required: "You must provide a password."
+        	},
+            confirmPassword: {
+        		required: "You must confirm your password."
+        	}
+        	
+		},
+        	
+        errorClass: "field-validation-error",
+        validClass: "valid"
+        		
+     });
+	
 
     // Email address - required
     // Email address - .gov only (BONUS)
