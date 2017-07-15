1596566cd86c48e096b33e89aa77f8d6a90e8068
Sahill Naik <sahillnaik@Sahills-MacBook-Pro.local>
2017-07-14 22:44:35 -0400

    test

--- m4-w10-history-geek/src/main/webapp/js/exercises/event-exerises.js
+++ m4-w10-history-geek/src/main/webapp/js/exercises/event-exerises.js
@@ -23,27 +23,5 @@ $(document).ready(function () {
 	
 		$('#grandtotal .price').html('$' + (subtotal + shippingCost));
 	});
-	
-	$(document).keydown(function(e){
-	    if (e.keyCode == 37) { 
-	       alert( "left pressed" );
-	       return false;
-	    }
-	    if (e.keyCode == 38) { 
-		       alert( "up pressed" );
-		       return false;
-		}
-	    if (e.keyCode == 39) { 
-		       alert( "right pressed" );
-		       return false;
-		}
-	    if (e.keyCode == 40) { 
-		       alert( "down pressed" );
-		       return false;
-		    }
-	    
-	});
-	
-	
 
 });
